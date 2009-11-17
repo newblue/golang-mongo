066423e96db3a799b708fa832f331eb629b94033
Ben Polidore <polidore@gmail.com>
2009-11-16 23:20:13 -0500

    groundwork laid out for bson types.

---
+++ BSON/types.go
@@ -0,0 +1,35 @@
+package bson
+
+type BsonTypeId int
+
+const (
+	Number	BsonTypeId	= 1;
+	String	BsonTypeId	= iota + 1;
+	Obj;
+	Array;
+	Binary;
+	Undefined;
+	Oid;
+	Boolean;
+	Date;
+	Null;
+	Regex;
+	Reference;
+	Code;
+	Symbol;
+	CodeWScope;
+	Integer;
+	Timestamp;
+	Long;
+	MinKey	BsonTypeId	= -1;
+	MaxKey	BsonTypeId	= 127;
+)
+
+type BsonType interface {
+  TypeNumber() BsonTypeId;
+}
+
+type BsonNumber float;
+func (num BsonNumber) TypeNumber() BsonTypeId {
+  return Number;
+}
---
+++ BSON/types_test.go
@@ -0,0 +1,16 @@
+package bson
+
+import (
+  "testing";
+)
+
+func TestBsonInt(t *testing.T) {
+  var testVar BsonType;
+  var num BsonNumber;
+  num = 0.0;
+  testVar = num;
+  typeNumber := testVar.TypeNumber();
+  if typeNumber != Number {
+    t.Errorf("Int failed, Expecting %d. Got %d",Number,typeNumber);
+  }
+}
